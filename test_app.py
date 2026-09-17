28586a8205e6dfcaff24bac5836672b27b0b057e
JaroslawPluska <jaroslaw.pluska@atos.ai>
2026-09-17 07:36:29 +0200

    fix test

--- test_app.py
+++ test_app.py
@@ -1,16 +1,25 @@
-#write test cases for app.py
-# to test flask api enpoint using the unit test framework and flask's test client   
 import unittest
+
 from app import app
+
 
 class FlaskAppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
 
-    def test_index(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
+    def test_total_revenue(self):
+        response = self.app.get("/total_revenue")
 
-if __name__ == '__main__':
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.get_json()["total_revenue"], int)
+
+    def test_highest_region(self):
+        response = self.app.get("/highest_region")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.get_json()["region"], str)
+        self.assertIsInstance(response.get_json()["total_sales"], int)
+
+if __name__ == "__main__":
     unittest.main()
